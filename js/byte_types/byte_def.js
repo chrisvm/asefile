676ace9492023d15baea856aa05d22eac8b3c23d
Christian Velez <christian.velez55@gmail.com>
2016-01-04 03:13:03 +0000

    added docstring for parse method

--- js/byte_types/byte_def.js
+++ js/byte_types/byte_def.js
@@ -23,27 +23,32 @@ ByteDef.prototype.define = function (name, def) {
     }, this);
 };
 
+/**
+ * Definition parses a file, calling the callback with the parsed object
+ * @param {string} def - the name of the definition
+ * @param {string} filePath - the path to the file to be parsed
+ * @param {function(err, parsed)} cb - the callback, recieves err and parsed
+ **/
 ByteDef.prototype.parse = function (def, filePath, cb) {
     var CHUNK_SIZE = 256,
         buffer = new Buffer(CHUNK_SIZE);
 
     var pairs = _.pairs(this.defs[def]), pIndex = 0, ret = {};
     fs.open(filePath, 'r', function (err, fd) {
-        if (err) cb(err, null);
+        if (err) return cb(err, null);
 
         function close() {
             fs.close(fd, function (err) {
-                cb(err, ret);
-            });
+                return cb(err, ret);
+            }); 
         }
 
         (function readChunk(last) {
             fs.read(fd, buffer, 0, CHUNK_SIZE , null, function (err, nread) {
-                if (err) cb(err, null);
+                if (err) return cb(err, null);
 
                 if (nread == 0 || pIndex >= pairs.length) {
-                    close();
-                    return;
+                    return close();
                 }
 
                 // add last buffer
