fd18feb97f93c6454ec580d3dbf6b88834beb16d
chrisvm <christian.velez5@upr.edu>
2016-01-16 15:25:22 -0400

    ignoring byte areas

--- test/ase_def_test.js
+++ test/ase_def_test.js
@@ -1,11 +1,13 @@
 var path = require('path'), jsDir = path.resolve(__dirname, '../js');
 var should = require('should'), ase_def = require(path.join(jsDir, 'byte_types/ase_def'));
+var ByteDef = require(path.join(jsDir, 'byte_types/byte_def')),
+    types = require(path.join(jsDir, 'byte_types/byte_types'));
 
 describe("ase_def.js", function () {
     describe("#parsing", function () {
-        var testFile = path.resolve(__dirname, 'tank.ase');
 
         it('should parse correctly a header', function (done) {
+            var testFile = path.resolve(__dirname, 'tank.ase');
             var testObj = {
                 file_size: 1678,
                 magic_num: 42464,
@@ -18,13 +20,14 @@ describe("ase_def.js", function () {
                 blank1: 0,
                 blank2: 0,
                 pallete_alpha_index: 0,
-                ignore: new Buffer(3),
-                num_colors: 256,
-                future: new Buffer(94)
+                num_colors: 256
             };
             ase_def.parse('header', testFile, function (err, parsed) {
                 if (err) throw err;
-                parsed.should.deepEqual(parsed, testObj);
+                parsed.should.have.properties(testObj);
+                done();
+            });
+        });
                 done();
             });
         });
